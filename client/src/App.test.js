ad80c9f11b2afe699b8203bef30dd4963aebda3b
Kabuswe <kabuswe.mulenga@carexs.com>
2021-12-04 09:23:35 +0100

    empty commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {});
+test('renders without crashing', () => {
+  
+});
